8f3d22b584d8be39511f7deff4e4006223a94651
MuhammadYasinSaleem <139649708+MuhammadYasinSaleem@users.noreply.github.com>
2025-05-26 23:25:22 +0500

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,32 +1,3 @@
-// import React from "react";
-// import Hero from "../components/Hero";
-// import Biography from "../components/Biography";
-// import Departments from "../components/Departments";
-
-// const Home = () => {
-//   return (
-//     <>
-//       <Hero
-//         title={
-//           <>
-//             Diagnosense Smarter Detection Healthier Futures
-//             <br />
-//             <span className="text-gray-600">
-//               AI-Powered Lung Cancer Detection You Can Trust
-//             </span>
-//           </>
-//         }
-//         imageUrl="/hero.png"
-//       />
-
-//       <Biography imageUrl={"/about.jpg"} />
-//       <Departments />
-//     </>
-//   );
-// };
-
-// export default Home;
-
 import React from "react";
 import Hero from "../components/Hero";
 import Biography from "../components/Biography";
